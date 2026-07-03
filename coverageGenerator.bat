f81a7aeb0326589917b81994c424a9314fec16bc
ShiryaeP <shiryaep@yandex.ru>
2026-07-03 21:25:14 +0300

    README.MD

--- coverageGenerator.bat
+++ coverageGenerator.bat
@@ -1,2 +1,6 @@
-dotnet test --collect:"XPlat Code Coverage" --results-directory ./TestResults
-reportgenerator "-reports:TestResults/**/coverage.cobertura.xml" "-targetdir:CoverageReport" -reporttypes:Html
+dotnet test --collect:"XPlat Code Coverage" --results-directory ./Tests/TestResults
+#dotnet tool install -g dotnet-reportgenerator-globaltool
+reportgenerator "-reports:Tests/TestResults/**/coverage.cobertura.xml" "-targetdir:Tests/CoverageReport" -reporttypes:Html
+pause
+# Before execution make sure you installed report generator
+#   Fix by uncomment dotnet tool install command
