6f1be1f6a8949fc6d0dfb333f244b4d81ffe2ab6
ShiryaeP <shiryaep@yandex.ru>
2026-07-07 15:34:50 +0300

    Всякие перемещения и попытка утрясти проект в его нормальное состояние

--- Services/YaEvents/Application/DTO/UpdateEventDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Application.DTO;
-
-public sealed record UpdateEventDto
-{
-    public string? Title { get; init; }
-    public DateTime? StartAt { get; init; }
-    public DateTime? EndAt { get; init; }
-    public string? Description { get; init; }
-}
